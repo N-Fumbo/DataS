0ea929a08ec39522cdeb6ff275027660c70b047a
Alexander <74.71.spartacus@gmail.com>
2023-06-18 21:20:28 +0300

    Добавьте файлы проекта.

---
+++ DataS/DataStructure/BT/BinaryTree.cs
@@ -0,0 +1,53 @@
+﻿using DataS.DataStructure.LList;
+
+namespace DataS.DataStructure.BT
+{
+    public class BinaryTree<T> where T : IComparable
+    {
+        public int Count { get; private set; }
+
+        private Node<T> _root;
+
+        public void Add(T value)
+        {
+            if (_root is null)
+            {
+                _root = new Node<T>(value);
+            }
+            else
+            {
+                var current = _root;
+
+                while (current is not null)
+                {
+                    if (value.CompareTo(current.Value) < 0)
+                    {
+                        if (current.Left is not null)
+                        {
+                            current = current.Left;
+                        }
+                        else
+                        {
+                            current.Left = new Node<T>(value);
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (current.Right is not null)
+                        {
+                            current = current.Right;
+                        }
+                        else
+                        {
+                            current.Right = new Node<T>(value);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Count++;
+        }
+    }
+}
